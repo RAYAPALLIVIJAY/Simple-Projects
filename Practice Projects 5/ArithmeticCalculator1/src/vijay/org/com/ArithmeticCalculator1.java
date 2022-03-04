7252e7b37a8d773813c710ca57c7bbc77c8bed3f
vijay <vijaygoud3333@gmail.com>
2022-03-04 16:00:17 +0530

    done

---
+++ Practice Projects 5/ArithmeticCalculator1/src/vijay/org/com/ArithmeticCalculator1.java
@@ -0,0 +1,25 @@
+             package vijay.org.com;                
+public class ArithmeticCalculator1 {
+                              public static void main(String[] args) {
+		  //Variables Definition and Initialization
+		  int number1 = 10, number2 = 5;
+		  //Addition Operation
+		  int sum = number1 + number2;
+		  System.out.println("Sum is: " + sum);
+		  //Subtraction Operation
+		  int dif = number1 - number2;
+		  System.out.println("Difference is : " + dif);
+		  //Multiplication Operation
+		  int mul = number1 * number2;
+		  System.out.println("Multiplied value is : " + mul);
+		  //Division Operation
+		  int div = number1 / number2;
+		  System.out.println("Quotient is : " + div);
+		  //Modulus Operation
+		  int rem = number1 % number2;
+		  System.out.println("Remainder is : " + rem);	
+}
+}
+
+
+
